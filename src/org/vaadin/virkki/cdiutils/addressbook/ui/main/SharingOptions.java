31eb081b37e6038b7805fac0006fada6739ba487
Tomi Virkki <virkki@vaadin.com>
2013-01-27 20:15:19 +0200

    Sharing options update

--- src/org/vaadin/virkki/cdiutils/addressbook/ui/main/SharingOptions.java
+++ src/org/vaadin/virkki/cdiutils/addressbook/ui/main/SharingOptions.java
@@ -1,9 +1,15 @@
 package org.vaadin.virkki.cdiutils.addressbook.ui.main;
 
+import javax.enterprise.event.Observes;
+import javax.enterprise.event.Reception;
 import javax.inject.Inject;
 
 import org.vaadin.virkki.cdiutils.addressbook.util.Lang;
 import org.vaadin.virkki.cdiutils.application.VaadinContext.VaadinScoped;
+import org.vaadin.virkki.cdiutils.componentproducers.Localizer;
+import org.vaadin.virkki.cdiutils.componentproducers.Preconfigured;
+import org.vaadin.virkki.cdiutils.mvp.CDIEvent;
+import org.vaadin.virkki.cdiutils.mvp.ParameterDTO;
 
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
@@ -16,22 +22,38 @@ import com.vaadin.ui.Window;
 public class SharingOptions extends Window {
     @Inject
     private Lang lang;
+    @Inject
+    @Preconfigured(labelValueKey = "sharingoptions-content")
+    private Label contentLabel;
+    @Inject
+    @Preconfigured(captionKey = "sharingoptions-gmail", implementation = CheckBox.class)
+    private Button gmailCheckBox;
+    @Inject
+    @Preconfigured(captionKey = "sharingoptions-mac", implementation = CheckBox.class)
+    private Button macCheckBox;
+    @Inject
+    @Preconfigured(captionKey = "ok")
+    private Button okButton;
 
     public final void init() {
         setModal(true);
         setWidth(50.0f, UNITS_PERCENTAGE);
         center();
-        setCaption(lang.getText("sharingoptions-caption"));
-        addComponent(new Label(lang.getText("sharingoptions-content")));
-        addComponent(new CheckBox(lang.getText("sharingoptions-gmail")));
-        addComponent(new CheckBox(lang.getText("sharingoptions-mac")));
-        final Button close = new Button(lang.getText("ok"));
-        close.addListener(new Button.ClickListener() {
+
+        addComponent(contentLabel);
+        addComponent(gmailCheckBox);
+        addComponent(macCheckBox);
+        okButton.addListener(new Button.ClickListener() {
             @Override
             public void buttonClick(final ClickEvent event) {
                 SharingOptions.this.close();
             }
         });
-        addComponent(close);
+        addComponent(okButton);
+    }
+
+    void localize(
+            @Observes(notifyObserver = Reception.IF_EXISTS) @CDIEvent(Localizer.UPDATE_LOCALIZED_VALUES) final ParameterDTO parameters) {
+        setCaption(lang.getText("sharingoptions-caption"));
     }
 }
